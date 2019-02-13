f552c06f46af9acd9a49da4e664b2de003612aae
Patrick Stegmann <wonderb0lt@users.noreply.github.com>
2019-02-13 22:12:48 +0100

    Fixing linter error

--- src/app/shared/services/scheduled-dispatch.service.ts
+++ src/app/shared/services/scheduled-dispatch.service.ts
@@ -36,7 +36,9 @@ export class ScheduledDispatchService extends OpenTTTService {
     return this.http.post<ScheduledDispatch>(this.url('scheduled-dispatches/'), dispatch);
   }
 
-  getSchedulesForDispatchByStation(id: number, numberOfDepartures: number = 5, withReturnOrder: boolean = false): Observable<ScheduleByStation[]> {
+  getSchedulesForDispatchByStation(id: number, 
+                                   numberOfDepartures: number = 5, 
+                                   withReturnOrder: boolean = false): Observable<ScheduleByStation[]> {
     return this.http.get<ScheduleByStation[]>(
       this.url(`scheduled-dispatches/${id}/departures-by-station`),
       {params: {numberOfDepartures: numberOfDepartures.toString(), withReturnOrder: withReturnOrder.toString()}}
