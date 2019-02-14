b435f4f502c50d4c304e59c57b5a0ea64218a574
Patrick Stegmann <patrick.stegmann@zuehlke.com>
2019-02-14 23:15:53 +0100

    WIP: Dynamic sidebar links, yay!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 
+interface SidenavLink {
+  routerLink: string;
+  featherIcon: any;
+  linkText: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  displayedLinks: SidenavLink[] = [
+    {routerLink: '/home', featherIcon: 'home', linkText: 'Home'},
+  ];
 }
