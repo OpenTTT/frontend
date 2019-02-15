4dd60b05a78cccddf8909af97d755cf44fbca497
Patrick Stegmann <patrick.stegmann@zuehlke.com>
2019-02-15 11:08:26 +0100

    WIP: Resurrected tag module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { StationListComponent } from './stations/station-list/station-list.component';
+import { TagListComponent } from './tags/tag-list/tag-list.component';
+import { NewTagComponent } from './tags/new-tag/new-tag.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'stations', component: StationListComponent},
+  {path: 'tags', component: TagListComponent},
+  {path: 'tags/new', component: NewTagComponent},
   /*
   {path: 'dispatches', component: DispatchListComponent},
   {path: 'dispatches/new', component: NewScheduledDispatchComponent},
   {path: 'dispatches/:id', component: DispatchDetailComponent},
   {path: 'timetables', component: TimetableListComponent},
   {path: 'timetables/:id', component: TimetableDetailComponent},
-  {path: 'tags', component: TagListComponent},
-  {path: 'tags/new', component: NewTagComponent},
  */
 ];
 
